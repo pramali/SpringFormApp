40de90bdaacf8ae837b1e6663deabeedbc5e37e6
Pramod Mali <57476794+pramali@users.noreply.github.com>
2021-12-13 01:10:50 +0530

    Added SOP Statement

--- src/main/java/com/ashok/controller/UserController.java
+++ src/main/java/com/ashok/controller/UserController.java
@@ -13,6 +13,7 @@ public class UserController {
 
 	@RequestMapping(value = "/displayForm.htm", method = RequestMethod.GET)
 	public String displayForm(Model model) {
+		System.out.println("Display Form Method Called ");
 		model.addAttribute("user", new User());
 		int i = 10;
 		return "userForm";
